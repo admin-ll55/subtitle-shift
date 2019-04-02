888312693ce8de481deb6c9c247bbdc1e2760e3d
admin-ll55 <36874774+admin-ll55@users.noreply.github.com>
2019-04-02 18:45:01 +0800

    Add files via upload

--- _S_S.py
+++ _S_S.py
@@ -4,27 +4,28 @@ import sys
 import traceback
 def shift(line):
   global offset
-  tsh = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[0][1])*3600000
-  tsm = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[0][2])*60000
-  tss = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[0][3])*1000
-  tsms = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[0][4])
+  m = re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)
+  tsh = int(m[0][1])*3600000
+  tsm = int(m[0][2])*60000
+  tss = int(m[0][3])*1000
+  tsms = int(m[0][4])
   ts = tsh+tsm+tss+tsms+offset
   tsh = int(ts//3600000)
   tsm = int((ts-tsh*3600000)//60000)
   tss = int((ts-tsh*3600000-tsm*60000)//1000)
   tsms = int((ts-tsh*3600000-tsm*60000-tss*1000))
-  ts = re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[0][0]
+  ts = m[0][0]
   line = line.replace(ts, str(tsh)+":"+str(tsm).zfill(2)+":"+str(tss).zfill(2)+"."+str(tsms).zfill(2))
-  teh = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[1][1])*3600000
-  tem = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[1][2])*60000
-  tes = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[1][3])*1000
-  tems = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[1][4])
+  teh = int(m[1][1])*3600000
+  tem = int(m[1][2])*60000
+  tes = int(m[1][3])*1000
+  tems = int(m[1][4])
   te = teh+tem+tes+tems+offset
   teh = int(te//3600000)
   tem = int((te-teh*3600000)//60000)
   tes = int((te-teh*3600000-tem*60000)//1000)
   tems = int((te-teh*3600000-tem*60000-tes*1000))
-  te = re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[1][0]
+  te = m[1][0]
   line = line.replace(te, str(teh)+":"+str(tem).zfill(2)+":"+str(tes).zfill(2)+"."+str(tems).zfill(2))
   return line
 try:
