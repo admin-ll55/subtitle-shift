1eef5e2788c032751cbbe9eadeace684f7f9f7ef
admin-ll55 <36874774+admin-ll55@users.noreply.github.com>
2019-04-02 18:39:18 +0800

    Add files via upload

---
+++ _S_S.py
@@ -0,0 +1,47 @@
+import os
+import re
+import sys
+import traceback
+def shift(line):
+  global offset
+  tsh = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[0][1])*3600000
+  tsm = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[0][2])*60000
+  tss = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[0][3])*1000
+  tsms = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[0][4])
+  ts = tsh+tsm+tss+tsms+offset
+  tsh = int(ts//3600000)
+  tsm = int((ts-tsh*3600000)//60000)
+  tss = int((ts-tsh*3600000-tsm*60000)//1000)
+  tsms = int((ts-tsh*3600000-tsm*60000-tss*1000))
+  ts = re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[0][0]
+  line = line.replace(ts, str(tsh)+":"+str(tsm).zfill(2)+":"+str(tss).zfill(2)+"."+str(tsms).zfill(2))
+  teh = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[1][1])*3600000
+  tem = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[1][2])*60000
+  tes = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[1][3])*1000
+  tems = int(re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[1][4])
+  te = teh+tem+tes+tems+offset
+  teh = int(te//3600000)
+  tem = int((te-teh*3600000)//60000)
+  tes = int((te-teh*3600000-tem*60000)//1000)
+  tems = int((te-teh*3600000-tem*60000-tes*1000))
+  te = re.findall("(([0-9]):([0-9][0-9]):([0-9][0-9])\.([0-9][0-9]))", line)[1][0]
+  line = line.replace(te, str(teh)+":"+str(tem).zfill(2)+":"+str(tes).zfill(2)+"."+str(tems).zfill(2))
+  return line
+try:
+  if len(sys.argv) < 2 or len(sys.argv) > 3:
+    raise("Invalid Arguments.")
+  ass = re.sub("\\r\\n", "\\n", open(sys.argv[1], 'rb').read().decode())
+  if len(sys.argv) == 2:
+    offset = int(input("Please input offset in ms: "))
+  else:
+    offset = int(sys.argv[2])
+  lines = ass.split("\n")
+  for x in range(0, len(lines)):
+    if re.search("^Dialogue:", lines[x]):
+      lines[x] = shift(lines[x])
+  os.rename(sys.argv[1], sys.argv[1]+".bak")
+  open(sys.argv[1], 'wb').write(("\n".join(lines)).encode())
+except:
+  traceback.print_exc()
+  input()
+sys.exit(0)
